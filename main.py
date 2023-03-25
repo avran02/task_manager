2303728aa51b4982d125300774c5f07fdc89c4d7
avran02 <avran2002@gmail.com>
2023-03-25 19:03:57 +0300

    Beta

--- main.py
+++ main.py
@@ -14,20 +14,17 @@ from kivy.uix.spinner import Spinner, SpinnerOption
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 
-
-
-
 from kivy.graphics import Rectangle, Ellipse, RoundedRectangle, Color
 from kivy.core.window import Window
 from kivy.properties import ListProperty
 
 
-
-
+Window.borderless = True
 Window.size = (400, 500)
 Window.clearcolor = (3/255, 53/255, 78/255, 1)
 
 Builder.load_file('styles.kv')
+
 
 class SpinnerOptions(SpinnerOption):
 
@@ -40,14 +37,15 @@ class SpinnerOptions(SpinnerOption):
 
 
 class MySpinner(Spinner):
+
     def __init__(self, **kwargs):
         super(MySpinner, self).__init__(**kwargs)
         self.option_cls = SpinnerOptions
 
+
 class MyTabbedPanelItem(TabbedPanelItem):
+
 	background_normal = ""
-
-
 
 
 class MyButton(Button):
@@ -56,20 +54,16 @@ class MyButton(Button):
 
 
 class MyLabel(Label):
-	pass
 
+	pass
 			
 
 class MyApp(App):
 
 	def __init__(self):
-		print(f'self : {self}')
 		super().__init__()
 		self.label = Label(text='Новая задача:', size_hint=(1, 0.1))
-		self.save_btn = MyButton(text = 'Сохранить', size_hint = (1, 0.1), background_normal = 'some/int_btn.png')
-
-
-		# self.save_btn.bind(on_press = Back.save_task)
+		self.save_btn = MyButton(text = 'Сохранить', size_hint = (1, 0.1), background_normal = 'Icons/int_btn.png')
 
 		self.writer = TextInput(size_hint = (1, 0.503))
 		self.label2 = Label(text = 'Удалить задачу:', size_hint = (1, 0.1))
@@ -77,38 +71,32 @@ class MyApp(App):
 		self.rm_selection = MySpinner(size_hint = (1, 0.07), text = 'Выберите из списка', background_normal = "")
 		Back.add_removing_el(self)
 
-
-		self.rm_btn = MyButton(text = 'Удалить', size_hint = (1, 0.1), background_normal = 'some/int_btn_rev.png')
+		self.rm_btn = MyButton(text = 'Удалить', size_hint = (1, 0.1), background_normal = 'Icons/int_btn_rev.png')
 
 		
-
-
 	def add_task_with_btn(self, state):
 		text = self.writer.text
 		if text != '':
 			if text[0] == ' ':
 				TXT.clear_txt()# а нужен ли этот метод он медленный
-                # QtWidgets.QMessageBox TODO: понять какого гуя зачеркивается всегда последняя задача
 			else:
 				TXT.clear_txt(self)
 				text = '0' + text
 				TXT.backup_tasks(self, text)
 				Back.add_task_to_screen(self,text)
-				
 				Back.add_removing_el(self)
-	
-	
+
 
 	def build(self):
 		self.title = 'task manager'
-		
+
 		self.tasks_tab = MyTabbedPanelItem(text = 'Список задач')
 		self.config_tab = MyTabbedPanelItem(text = 'Управление')
 		self.tabs = TabbedPanel(default_tab = self.config_tab)
-		self.tasks_tab.background_normal = 'some/my_tab.xcf'
-		self.tasks_tab.background_down = 'some/int_btn.png'
-		self.config_tab.background_normal = 'some/my_tab.xcf'
-		self.config_tab.background_down = 'some/int_btn.png'
+		self.tasks_tab.background_normal = 'Icons/my_tab.xcf'
+		self.tasks_tab.background_down = 'Icons/int_btn.png'
+		self.config_tab.background_normal = 'Icons/my_tab.xcf'
+		self.config_tab.background_down = 'Icons/int_btn.png'
 
 		self.box = BoxLayout(orientation = 'vertical', spacing = 5)
 		self.box.add_widget(self.label)
@@ -125,94 +113,48 @@ class MyApp(App):
 		self.tabs.add_widget(self.tasks_tab)
 		self.tabs.add_widget(self.config_tab)
 
-
 		Back.func(self)
 		self.save_btn.bind(on_press = self.add_task_with_btn)
 		self.rm_btn.bind(on_press = self.rm_task_from_tab)
 
-
 		return self.tabs
 
 
-		# def rm_task_from_tab(self):
-	# 	for i in reversed(range(self.verticalLayout.count())):
-	# 		self.verticalLayout.itemAt(i).widget().deleteLater()
-	# 	text = self.rm_task.currentText()
-	# 	with open('tasks.txt','r') as tasks:
-	# 		lines = ''
-	# 		for line in tasks:
-	# 			if line[1:-1] == text:
-	# 				pass
-	# 			else:
-	# 				lines += line
-	# 	with open('tasks.txt', 'w') as tasks:
-	# 		tasks.write(lines)
-	# 	self.rm_task.clear()
-	# 	self.add_removing_el()
-	# 	try:
-	# 		self.init_tasks()
-	# 	except AttributeError:
-	# 		print('deliteing error')#TODO: Qmessage
-
 	def rm_task_from_tab(self, state):
-		print(f'grid before: {self.grid}')
 		text = self.rm_selection.text
-		# print(f'text is {text}', f'lenth: {len(text)}')
 		with open ('tasks.txt', 'r') as tasks:
 			lines = ''
 			for line in tasks:
-				# print(line[1:-1], f'lenth: {len(line[1:-1])}')
 				if line[1:-1] == text:
-					print('was founded')
 					pass
 				else:
 					lines += line
 		with open ('tasks.txt', 'w') as tasks:
 			tasks.write(lines)
-
 		for wid in self.grid:
-			print(self.grid[wid].text[3:-4])
 			if self.grid[wid].text[3:-4] == text:
-				print('Найдено')
 				self.tasks.remove_widget(wid)
 				self.tasks.remove_widget(self.grid[wid]) #todo: убрать утечку памяти
-
-		print(f'grid after: {self.grid}')
 		Back.add_removing_el(self)
 		self.rm_selection.text = 'Выберите из списка'
 
 
-
-class Back(App):
+class Back():
 	MyApp.grid = dict()
 
 	def func(self):
 		Back.init_tasks(self)
 
 
-
-
 	def checkbox_callback(self, value):
-		
 		self.cb_label = MyApp.grid[self]
 		text = self.cb_label.text[3:-4]
-		
-		print(text)
 		if value:
-			
-			print("Checkbox is checked")
 			self.cb_label.text = f'[s]{text}[/s]'
 			TXT.set_checked_in_txt(self, text)
-			print('seccess!')
-
 		else:
-			print("Checkbox is unchecked")
 			self.cb_label.text = f'[i]{text}[/i]'
-			print('seccess!')
 			TXT.set_unchecked_in_txt(self, text)
-	
-
-
 
 
 	def init_tasks(self): #text <= 42 el
@@ -226,24 +168,18 @@ class Back(App):
 
 	def add_task_to_screen(self, text):
 		self.checkBox = CheckBox(width = 50, size_hint_x = None)
-		
 		self.tasks.add_widget(self.checkBox)
-		
-		
 		self.writer.text = ''
 		if text[0] == '0':
 			self.checkBox.active = False
 			self.cb_label = MyLabel(markup = True)
 			self.cb_label.text = f'[i]{text[1:]}[/i]'
-			
 		else:
 			self.checkBox.active = True
 			self.cb_label = MyLabel(markup = True)
 			self.cb_label.text = f'[s]{text[1:]}[/s]'
-			
 		self.grid[self.checkBox] = self.cb_label
 		self.tasks.add_widget(self.cb_label)
-		# print(self.grid)
 		self.checkBox.bind(active = Back.checkbox_callback)
 
 
@@ -257,25 +193,19 @@ class Back(App):
 					lines += (line[1:-1], )
 			self.rm_selection.values = lines
 		
-	
-		
-
 
 class TXT(Back):
-	
 	def set_checked_in_txt(self, text):
 			with open('tasks.txt','r') as tasks:
 				lines = ''
 				for line in tasks:
 					if line[1:-1] == text:
-						
 						list_line = list(line)
 						list_line[0] = '1'
 						line = ''.join(list_line)
 						lines += line
 					else:
 						lines += line
-
 					with open('tasks.txt', 'w') as tasks:
 						tasks.write(lines)
 
@@ -285,17 +215,14 @@ class TXT(Back):
 				lines = ''
 				for line in tasks:
 					if line[1:-1] == text:
-						
 						list_line = list(line)
 						list_line[0] = '0'
 						line = ''.join(list_line)
 						lines += line
 					else:
 						lines += line
-
 					with open('tasks.txt', 'w') as tasks:
 						tasks.write(lines)
-
 
 
 	def clear_txt(self):
@@ -310,7 +237,6 @@ class TXT(Back):
 			tasks.write(lines)
 
 
-
 	def backup_tasks(self, text):
 		with open('tasks.txt', 'a') as tasks:
 			tasks.write(f'{text}\n')
@@ -318,5 +244,4 @@ class TXT(Back):
 
 
 if __name__ == '__main__':
-
 	MyApp().run()		
